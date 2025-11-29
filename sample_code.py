1514b5f1f25a3582d4390578f28bd6ae1fd613ed
nootalmuraikhi-creator <nootalmuraikhi@gmail.com>
2025-11-29 18:25:20 +0300

    Update sample_code.py

--- sample_code.py
+++ sample_code.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 user_input = input("Enter a command to run: ")
 subprocess.run(user_input, shell=False)
-USERNAME = "Noot"  
-PASSWORD = "12345"
+import os
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 subprocess.call("ls -la", shell=True)
