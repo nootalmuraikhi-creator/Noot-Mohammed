c53d6c8dc64a3ca359cd91da654afaa7dc8544bb
nootalmuraikhi-creator <nootalmuraikhi@gmail.com>
2025-11-29 18:24:25 +0300

    Update sample_code.py

--- sample_code.py
+++ sample_code.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 user_input = input("Enter a command to run: ")
-os.system(user_input)
-USERNAME = "Noot"  # تم تغيير "admin" إلى "Noot"
+subprocess.run(user_input, shell=False)
+USERNAME = "Noot"  
 PASSWORD = "12345"
 subprocess.call("ls -la", shell=True)
