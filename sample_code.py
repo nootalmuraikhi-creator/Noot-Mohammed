ffa136c783a95d785dab9199d558f8fcf0da41ae
nootalmuraikhi-creator <nootalmuraikhi@gmail.com>
2025-11-29 18:30:43 +0300

    Update sample_code.py

--- sample_code.py
+++ sample_code.py
@@ -6,3 +6,11 @@ import os
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 subprocess.call("ls -la", shell=True)
+allowed_commands = ["ls", "pwd", "whoami"]  
+
+user_input = input("Enter a command to run: ")
+
+if user_input in allowed_commands:
+    subprocess.run(user_input, shell=False)
+else:
+    print("Command not allowed!")
